9b78ef410f6475313b6402100520eb655be9d8b2
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-19 20:33:44 +0100

    component in compontent

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const ApprovalCard = props => {
+    console.log(props)
     return (
         <div className="ui card">
-            <div className="content"> Are you Sure?</div>
+            <div className="content"> {props.children}</div>
             <div className="extra content">
                 <div className="ui two buttons">
                     <div className="ui basic green button">Approve</div>
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import faker from 'faker';
 const App = () => {
     return (
         <div className="ui container comments">
-           <ApprovalCard></ApprovalCard>
-           
-           <CommentDetail  author="Sam" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
+           <ApprovalCard>
+                <CommentDetail  author="Sam" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
+           </ApprovalCard>
            <CommentDetail author="Alex" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
            <CommentDetail author="Jane" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
         </div>
