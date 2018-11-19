deb9f6ed3522fe3b54278c99aac592a18d8152b8
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-19 20:30:54 +0100

    another component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
+           <ApprovalCard></ApprovalCard>
+           
            <CommentDetail  author="Sam" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
            <CommentDetail author="Alex" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
            <CommentDetail author="Jane" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
