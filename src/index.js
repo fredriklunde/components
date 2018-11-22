02a4815ab4b3eb40a8b4768f645569db8c7bb2d4
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-22 17:00:06 +0100

    added header

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import faker from 'faker';
 
 const App = () => {
     return (
-        <div className="ui container comments">
-           <ApprovalCard>
-                <CommentDetail  author="Sam" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
-           </ApprovalCard>
-           <ApprovalCard>
-                <CommentDetail author="Alex" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
-           </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author="Jane" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
-           </ApprovalCard>
+        <div className="app header">
+            <h1> Welcome to the app </h1>
+            <div className="ui container comments">
+                <ApprovalCard>
+                    <CommentDetail  author="Sam" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetail author="Alex" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetail author="Jane" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
+                </ApprovalCard>
+            </div>
         </div>
     );
 };
