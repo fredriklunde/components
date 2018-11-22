fa60867d1c5df99b69d7bab068c83620cbf54848
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-22 17:03:06 +0100

    header

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import faker from 'faker';
 
 const App = () => {
     return (
-        <div className="app header">
-            <h1> Welcome to the app </h1>
+        <div className="app">
+            <h2 className="ui center aligned icon header">
+            <i className="circular users icon"></i>
+            Approval Cards
+            </h2>   
             <div className="ui container comments">
                 <ApprovalCard>
                     <CommentDetail  author="Sam" timeAgo="today at 5:54OM" avatar={faker.image.avatar()} text="testar" />
