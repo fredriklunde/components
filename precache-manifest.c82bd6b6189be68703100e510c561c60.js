9f22b6b0f4096867922ac725c45a898d35857276
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-20 10:56:54 +0100

    Updates

---
+++ precache-manifest.c82bd6b6189be68703100e510c561c60.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "578b3ffda55358b960e7",
+    "url": "/static/js/main.578b3ffd.chunk.js"
+  },
+  {
+    "revision": "7a0c9998d67029e6661a",
+    "url": "/static/js/1.7a0c9998.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7fd6591df9f626d33cebb32bc17d41c6",
+    "url": "/index.html"
+  }
+];
